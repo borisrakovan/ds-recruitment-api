508018db00249257326f1cb5454225c824820eaf
Rakovan, Boris <rakovbor@fel.cvut.cz>
2022-01-07 00:35:12 +0100

    create basic marshmallow schemas

---
+++ app/schema.py
@@ -0,0 +1,26 @@
+from app import ma
+from app.models import Candidate, JobAdvertisement
+
+
+class CandidateSchema(ma.SQLAlchemyAutoSchema):
+    """Schema for the Candidate model."""
+    class Meta:
+        model = Candidate
+        # fail-safe selection of model fields
+        fields = ("id", "first_name", "surname", "email", "phone_number",
+                  "expected_salary", "skills", "advertisement",)
+
+
+class JobAdvertisementSchema(ma.SQLAlchemyAutoSchema):
+    """Schema for the JobAdvertisement model."""
+    class Meta:
+        model = JobAdvertisement
+
+        fields = ("id", "title", "salary_min", "salary_max", "full_text",)
+
+
+"""Schema object declarations"""
+candidate_schema = CandidateSchema()
+candidates_schema = CandidateSchema(many=True)
+advertisement_schema = JobAdvertisementSchema()
+advertisements_schema = JobAdvertisementSchema(many=True)
